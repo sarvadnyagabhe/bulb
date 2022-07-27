28dc05227268ab22e3182098bb4e1d6713b970d0
sarvadnyagabhe <sarvadnyagabhe@gmail.com>
2022-07-27 16:39:25 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// turn on function
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/bulb-on.jpg")
+} 
+function bulbOff(){
+    let bulbobj =document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/bulb-off.jpg")
+}
